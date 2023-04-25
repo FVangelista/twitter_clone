ba0e1ecb5c3ca7e0ad2df265ee258bf8a2ddf49e
fvangelista <bortoletti.filippo95@gmail.com>
2023-04-25 22:24:38 +0200

    feat: link

--- src/components/atoms/menu/Menu.jsx
+++ src/components/atoms/menu/Menu.jsx
@@ -1,11 +1,17 @@
-import MenuContent from "../menuContent";
+import { RiGithubLine } from "react-icons/ri";
 import "./index.css";
+import MenuContent from "../menuContent";
 
 const Menu = ({ setModalVisible }) => {
   return (
     <div className="Menu">
       <MenuContent setModalVisible={setModalVisible} />
-      <p className="copy">&copy; by filippo</p>
+      <p className="copy">
+        &copy; by filippo{" "}
+        <a href="https://github.com/FVangelista">
+          <RiGithubLine className="icon" />
+        </a>
+      </p>
     </div>
   );
 };
